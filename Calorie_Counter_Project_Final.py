9a1bf412593ed8416ee199418032f1274ff22320
rwill97 <76223951+rwill97@users.noreply.github.com>
2021-08-07 18:56:27 -0400

    Add files via upload

--- Calorie_Counter_Project_Final.py
+++ Calorie_Counter_Project_Final.py
@@ -3,25 +3,25 @@ name = input ("Enter name:")
 height = float(input ("Enter height in inches:"))
 weight = float(input ("Enter weight in lbs:"))
 age = float(input ("Enter age:"))
-sex = input ("Enter sex:")
+sex = input ("Enter Male or Female:")
 calories_per_day_male = 66.47 + 6.24*(weight) + 12.7*(height) - 6.755*age
 calories_per_day_female = 655.1 + 4.35*weight + 4.7*height - 4.7*age
-if sex == "male".lower():
+if sex.lower() == "male":
     print("Your BMR is estimated to be " + str(calories_per_day_male) + " per day.")
-if sex == "female".lower():
+if sex.lower() == "female":
     print("Your BMR is estimated to be " + str(calories_per_day_female) + " per day.")
 print("Now let's find out if you want to lose weight or gain weight.")
 goal = input ("Enter Lose or Gain:")
-if goal == "Gain".lower() and sex == "male".lower():
+if goal.lower() == "gain" and sex.lower() == "male":
     calorie_goal = calories_per_day_male + 500
     print("Your daily calorie goal to gain 1lb a week is " + str(calorie_goal) + ".")
-if goal == "Lose".lower() and sex == "male".lower():
+if goal.lower() == "lose" and sex.lower() == "male":
     calorie_goal = calories_per_day_male - 500
     print("Your daily calorie goal to lose 1lb a week is " + str(calorie_goal) + ".")
-if goal == "Gain".lower() and sex == "female".lower():
+if goal.lower() == "gain" and sex.lower() == "female":
     calorie_goal = calories_per_day_female + 500
     print("Your daily calorie goal to gain 1lb a week is " + str(calorie_goal) + ".")
-if goal == "Lose".lower() and sex == "female".lower():
+if goal.lower() == "lose" and sex.lower() == "female":
     calorie_goal = calories_per_day_female - 500
     print("Your daily calorie goal to lose 1lb a week is " + str(calorie_goal) + ".")
     
